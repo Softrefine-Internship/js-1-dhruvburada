63703830b51f4a1d2ad1870b2ac1b79b21dd084c
dhruvburada <buradadhruv35@gmail.com>
2025-07-09 11:10:58 +0530

    test started at 10:30

--- program-1.js
+++ program-1.js
@@ -1,1 +1,11 @@
 // Write a JavaScript program to clone an array.
+
+let array = [1, 2, 3, 4, 5];
+
+let copyarray = [];
+
+for (let i = 0; i < array.length; i++) {
+  copyarray[i] = array[i];
+}
+
+console.log(copyarray);
--- program-2.js
+++ program-2.js
@@ -2,3 +2,24 @@
 
 // Sample Input: [1, 2, [3, 4], [5, [6, 7]]];
 // Expected Output: [1, 2, 3, 4, 5, 6, 7];
+
+function flatArray(arr) {
+  let flatarray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const item = arr[i];
+
+    if (Array.isArray(item)) {
+      const nested = flatArray(item); // recursive flatten
+      for (let j = 0; j < nested.length; j++) {
+        flatarray.push(nested[j]);
+      }
+    } else {
+      flatarray.push(item); // add primitive directly
+    }
+  }
+
+  return flatarray;
+}
+
+console.log(flatArray([1, 2, [3, 4], [5, [6, 7]]]));
